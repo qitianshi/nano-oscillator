282446052f72a613378d1f01d4dc5496ca273be4
qitianshi <65122416+qitianshi@users.noreply.github.com>
2021-12-03 16:23:49 +0800

    Add get_result

--- analysis/parse_output.py
+++ analysis/parse_output.py
@@ -1,2 +1,19 @@
-# parse_output.py
-# Parses table.txt output from mumax3.
+"""Parses table.txt output from mumax3."""
+
+
+import os
+
+def get_result(date: str = None):
+
+    """Retrieves the contents of table.txt from the requested directory.
+
+    Args:
+        date (str): The date of the simulation, matching the name of the
+            subdirectory.
+    """
+
+    results_path = os.path.join(os.path.dirname(__file__), os.pardir, 'results')
+    requested_result = date if date is not None else sorted(os.listdir(results_path))[-1]
+
+    return open(os.path.join(results_path, requested_result, 'raw.out/table.txt'),
+                "r", encoding="utf-8").read()
