84df107f44b533b0e10d9ab31a0e9352c844c776
anweiteck <74035556+anweiteck@users.noreply.github.com>
2021-12-07 16:05:56 +0800

    Split get_result into 2 functions

--- analysis/parse_output.py
+++ analysis/parse_output.py
@@ -1,9 +1,9 @@
 """Parses table.txt output from mumax3."""
 
-
+import pandas as pd
 import os
 
-def get_result(date: str = None):
+def get_folder_dir(date: str = None):
 
     """Retrieves the path of table.txt from the requested directory.
 
@@ -15,4 +15,13 @@ def get_result(date: str = None):
     results_path = os.path.join(os.path.dirname(__file__), os.pardir, 'results')
     requested_result = date if date is not None else sorted(os.listdir(results_path))[-1]
 
-    return os.path.join(results_path, requested_result, 'raw.out/table.txt')
+
+    return os.path.join(results_path, requested_result)
+
+def get_result():
+    folder_dir = get_folder_dir()
+    return os.path.join(folder_dir, 'raw.out/table.txt')
+
+
+
+
