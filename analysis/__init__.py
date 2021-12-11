341a919fa279ed11a1aa075545358811844af011
qitianshi <65122416+qitianshi@users.noreply.github.com>
2021-12-11 10:19:07 +0800

    Add detailed package docs

--- analysis/__init__.py
+++ analysis/__init__.py
@@ -1,1 +1,14 @@
-"""Runs data analysis and plotting functions for output from mumax3."""
+"""Analysis, plotting, read/write, parsing, and data check functions for
+results from mumax3.
+
+Terminology used in this package:
+* data: A single `*.tsv` file containing either mumax3 output or calculated
+  data.
+* dataset: A folder, in any location, containing zero, one, or more data files.
+* raw: Raw output from mumax3, either in its original `table.txt` form or in
+  the converted `table.tsv` form.
+* result: A folder at the top level of the `results` directory. Its name
+  follows the convention "YYYY-MM-DD_hhmm".
+* split: Breaking raw data into several smaller data files, which are then
+  saved to disk in the appropriate result folder.
+"""
