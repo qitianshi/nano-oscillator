7d831ac710813cf7e469a35ca6888f973a337bd2
qitianshi <65122416+qitianshi@users.noreply.github.com>
2021-12-12 23:45:26 +0800

    Fix syntactic error

--- scripts/convert_raw_txt.sh
+++ scripts/convert_raw_txt.sh
@@ -12,5 +12,5 @@ source scripts/activate_py.sh
 python - $1 << PYSCRIPT
 import sys
 from analysis.readresults import convert_raw_txt
-convert_raw_txt(sys.argv[1] if len (sys.argv) >= 2 else None)
+convert_raw_txt(sys.argv[1] if len(sys.argv) >= 2 else None)
 PYSCRIPT
