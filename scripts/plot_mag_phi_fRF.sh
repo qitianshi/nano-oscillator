ace03c3dacafc0f4e9674a884187a35cfb41d6eb
qitianshi <65122416+qitianshi@users.noreply.github.com>
2021-12-15 13:19:10 +0800

    Add plot_mag_phi_fRF.sh

---
+++ scripts/plot_mag_phi_fRF.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+if  [[ $1 = "-h" ]]; then
+    cat << HELP
+usage: $0 [-h] <date>
+HELP
+    exit
+fi
+
+source scripts/activate_py.sh
+
+python - $1 << PYSCRIPT
+from os.path import join
+import sys
+
+from analysis.plot import plot_dataset_xy
+from analysis.readresults import read_dataset, dataset_dir, result_dir
+
+plot_dataset_xy(
+    data=read_dataset(dataset_dir(sys.argv[1], {"phi": None, "f_RF": None})),
+    x_var="t",
+    y_vars=["mx", "my", "mz"],
+    xlabel="t (s)",
+    save_to_root=join(result_dir(sys.argv[1]), "plots", "phi, f_RF")
+)
+
+PYSCRIPT
