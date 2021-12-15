93f66eadf849a70016f884392bb88c9245bdc870
anweiteck <74035556+anweiteck@users.noreply.github.com>
2021-12-15 17:44:59 +0800

    Fixed pep8

--- analysis/find_amp.py
+++ analysis/find_amp.py
@@ -1,16 +1,15 @@
 """Find amplitude from dataset"""
 
+from os.path import join
 import pandas as pd
 import numpy as np
 import readresults
-import matplotlib.pyplot as plt
-import plot
-from os.path import join
 
-def amp(
+
+def find_amp(
         date : str = None,
         phi : str = None,
-        f_RF : float = None,
+        fRF : float = None,
         var : str = None
 ):
     """Finds the amplitudes for one variable"""
@@ -19,14 +18,15 @@ def amp(
 
     waveform = readresults.read_data(readresults.data_path(date,
         {"phi": f"{phi:03}deg",
-        "f_RF": f"{f_RF / 10**9}GHz"}
+        "f_RF": f"{fRF / 10**9}GHz"}
     ))
 
-    amp = (waveform.loc[waveform["t"] > timeskip, var].max() -
+    # Super rudimentary way of determining amplitude
+    amplitude = (waveform.loc[waveform["t"] > timeskip, var].max() -
         waveform.loc[waveform["t"] > timeskip, var].min())/2
-    #super rudimentary way of determining amplitude
 
-    return amp
+    return amplitude
+
 
 def amp_phi_fRF(date : str = None, var : str = None):
     """Finds the ampltiude for all the split datasets, for one given var"""
@@ -35,18 +35,16 @@ def amp_phi_fRF(date : str = None, var : str = None):
 
     for phi in data["phi"].unique():
 
-        amplitude = np.empty((0, 2), float) #creates an empty numpy array
+        # Creates an empty numpy array
+        amplitude = np.empty((0, 2), float)
 
-        for f_RF in data["f_RF"].unique():
-            row = np.array([f_RF, amp(date, phi, f_RF, var)])
+        # Appends the amplitude and frequency values into the numpy 2D array
+        for fRF in data["f_RF"].unique():
+            row = np.array([fRF, find_amp(date, phi, fRF, var)])
             amplitude = np.append(amplitude, [row], axis=0)
-        #appends the amplitude and frequency values into the numpy 2D array
 
-        df = pd.DataFrame(amplitude, columns=["f_RF", "amplitude"])
-        df.to_csv(join(
+        # Converts the 2D array into a dataframe and csv file
+        output = pd.DataFrame(amplitude, columns=["f_RF", "amplitude"])
+        output.to_csv(join(
             readresults.result_dir(date), 'amplitudes', f"{phi}deg.tsv"), sep='\t', index=False
         )
-        #converts the 2D array into a dataframe and csv file
-
-
-
--- analysis/readresults.py
+++ analysis/readresults.py
@@ -71,6 +71,8 @@ def data_path(date: str = None, vals: dict[str, str] = None) -> str:
         return os.path.join(dataset_dir(date, vals), ", ".join(vals.values()) + ".tsv")
 
 def amplitude_path(date : str = None, phi : str = None):
+    """Returns the path of the amplitude data."""
+
     amplitude_dir = os.path.join(result_dir(date), "amplitudes")
     return os.path.join(amplitude_dir, f"{phi}deg.tsv")
 
