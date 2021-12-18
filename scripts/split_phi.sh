be914a19c89f0317315e4847cabe154b6e94fc11
qitianshi <65122416+qitianshi@users.noreply.github.com>
2021-12-18 19:06:14 +0800

    Remove reset_t parameter

--- scripts/split_phi.sh
+++ scripts/split_phi.sh
@@ -2,17 +2,17 @@
 
 if [[ $1 = "-h" ]]; then
     cat << HELP
-usage: $0 [-h] <date> <reset_t>
+usage: $0 [-h] <date>
 HELP
     exit
 fi
 
 source scripts/activate_py.sh
 
-python - $1 $2 << PYSCRIPT
+python - $1 << PYSCRIPT
 import sys
 from analysis.split import split_phi
 
-split_phi(sys.argv[1], sys.argv[2])
+split_phi(sys.argv[1])
 
 PYSCRIPT
