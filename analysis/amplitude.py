4584cc8c7c265c6efbe89edc9d3e66e817b99723
qitianshi <65122416+qitianshi@users.noreply.github.com>
2021-12-19 20:55:17 +0800

    Change amplitude functions to always use all magnetization components

--- analysis/amplitude.py
+++ analysis/amplitude.py
@@ -8,13 +8,13 @@ import numpy as np
 from analysis import read, paths
 
 
-def calc_amp(date: str, phi: str, fRF: float, mag_var: str) -> float:
+def __calc_amp(date: str, phi: str, fRF: float, mag_var: str) -> float:
     """
     Finds the amplitudes for one variable.
 
     Args:
-        var (str): The magnetization vector variable to calculate. Acceptable
-          values: "mx", "my", "mz".
+        mag_var (str): The magnetization vector variable to calculate.
+          Acceptable values: "mx", "my", "mz".
     """
 
     skip_duration = 1.5e-9
@@ -32,43 +32,48 @@ def calc_amp(date: str, phi: str, fRF: float, mag_var: str) -> float:
     return amplitude
 
 
-def amp_phi_fRF(mag_var: str, date: str = None):
-    """Finds the amplitude for all the split datasets, for one given var."""
+def amp_phi_fRF(date: str = None):
+    """Finds the amplitude for all the split datasets, for all magnetization
+    components.
+    """
 
     date = date if date is not None else paths.latest_date()
     data = read.read_data(paths.data_path(date))
 
-    amplitudes = np.empty((data["f_RF"].nunique(), 0), float)
-    freq_col = np.empty((0, 1), float)
+    for mag_var in ["mx", "my", "mz"]:
 
-    #creates a numpy array of all the frequency values
-    for fRF in data["f_RF"].unique():
-        row = np.array([fRF])
-        freq_col = np.append(freq_col, [row], axis=0)
+        amplitudes = np.empty((data["f_RF"].nunique(), 0), float)
+        freq_col = np.empty((0, 1), float)
 
-    amplitudes = np.column_stack((amplitudes, freq_col))
-
-    #creates a numpy array of all the amplitude data for each phi value
-    for phi in data["phi"].unique():
-
-        col = np.empty((0, 1), float)
-
+        #creates a numpy array of all the frequency values
         for fRF in data["f_RF"].unique():
-            row = np.array([calc_amp(date, phi, fRF, mag_var)])
-            col = np.append(col, [row], axis=0)
+            row = np.array([fRF])
+            freq_col = np.append(freq_col, [row], axis=0)
 
-        amplitudes = np.column_stack((amplitudes, col))
+        amplitudes = np.column_stack((amplitudes, freq_col))
 
-    #converts the numpy array into a dataframe
-    amplitude_data = pd.DataFrame(
-        amplitudes, columns=["f_RF", *[f"{i}deg" for i in data["phi"].unique()]])
-    amplitude_data.to_csv(paths.amplitude_path(mag_var, date), sep='\t', index=False)
+        # Creates a numpy array of all the amplitude data for each phi value.
+        for phi in data["phi"].unique():
+
+            col = np.empty((0, 1), float)
+
+            for fRF in data["f_RF"].unique():
+                row = np.array([__calc_amp(date, phi, fRF, mag_var)])
+                col = np.append(col, [row], axis=0)
+
+            amplitudes = np.column_stack((amplitudes, col))
+
+        read.prep_dir(join(paths.result_dir(date), "calculated_values"), clear=False)
+
+        # Outputs amplitude data.
+        pd.DataFrame(amplitudes, columns=["f_RF", *[f"{i}deg" for i in data["phi"].unique()]]) \
+            .to_csv(paths.amplitude_path(mag_var, date), sep='\t', index=False)
 
 
-def max_amp_phi(date: str = None, mag_vars: list[str] = None):
+def max_amp_phi(date: str = None):
     """Finds the maximum amplitudes for each phi value"""
 
-    mag_vars = mag_vars if mag_vars is not None else ["mx", "my", "mz"]
+    mag_vars = ["mx", "my", "mz"]
     date = date if date is not None else paths.latest_date()
     data = read.read_data(paths.data_path(date))
 
@@ -78,10 +83,11 @@ def max_amp_phi(date: str = None, mag_vars: list[str] = None):
         phi_col = np.append(phi_col, [phi], axis=0)
 
     for var in mag_vars:
+
         data = read.read_data(paths.amplitude_path(var, date))
         max_col = np.empty((0, 1), float)
 
-        #create numpy arrays with max amp and phi values
+        # Creates numpy arrays with MaxAmp and phi values.
         for val in (data.columns):
             if "deg" in val:
                 max_amp = np.array([data[val].max()])
@@ -89,9 +95,8 @@ def max_amp_phi(date: str = None, mag_vars: list[str] = None):
 
         phi_col = np.column_stack((phi_col, max_col))
 
-    #output to panda dataframe and a tsv file
-    output_data = pd.DataFrame(phi_col, columns=["phi"] + mag_vars)
-    output_data.to_csv(join(
+    # Outputs to data file.
+    pd.DataFrame(phi_col, columns=["phi"] + mag_vars).to_csv(join(
         paths.result_dir(date), 'calculated_values', 'MaxAmp.tsv'),
         sep='\t', index=False
     )
