d930e835c49b770e6c0da33f391a805f6f9be6d2
qitianshi <65122416+qitianshi@users.noreply.github.com>
2021-12-19 17:35:08 +0800

    Add plots_agg_dir function

--- analysis/paths.py
+++ analysis/paths.py
@@ -68,3 +68,9 @@ def amplitude_path(mag_var: str, date: str = None) -> str:
     """Returns the path of the amplitude data."""
 
     return os.path.join(result_dir(date), "calculated_values", f"amplitudes_{mag_var}.tsv")
+
+
+def plots_agg_dir(date: str = None) -> str:
+    """Returns the path of aggregate plots."""
+
+    return os.path.join(result_dir(date), "plots", "aggregate")
