761e46af7f95725ec156c2b1c59e4ac8f92dfb20
qitianshi <65122416+qitianshi@users.noreply.github.com>
2021-12-23 18:25:19 +0800

    Add curve fit calculation to __main__

--- analysis/__main__.py
+++ analysis/__main__.py
@@ -30,6 +30,11 @@ print("Calculating amplitude data...")
 anl.amplitude.amp_phi_fRF(DATE)
 anl.amplitude.max_amp_phi(DATE)
 
+# Calculates curve-fitting parameters for amplitude
+print("Calculating curve-fit for amplitudes")
+for mag_var in ["mx", "my", "mz"]:
+    anl.fit.fit_cauchy(mag_var=mag_var, xlim=[3.5e9, 6.0e9], date=DATE)
+
 #endregion
 
 #region Plotting
