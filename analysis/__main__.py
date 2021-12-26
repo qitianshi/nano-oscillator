c3d6377f353798d7acde3b1cc9fe745d44a7281b
qitianshi <65122416+qitianshi@users.noreply.github.com>
2021-12-26 17:54:05 +0800

    Add overlay of raw data points

--- analysis/__main__.py
+++ analysis/__main__.py
@@ -153,19 +153,28 @@ def __plotcheck_fitted_amp():
     for mag_var in ["mx", "my", "mz"]:
 
         curve_data = anl.read.read_data(anl.paths.fitted_amp_path(mag_var))
+        raw_data = anl.read.read_data(anl.paths.amp_path(mag_var, DATE))
+        rows = curve_data["phi"][::(len(curve_data["phi"]) // 4)]     # Extracts a few sample rows.
+        domain = (3.5e9, 6.0e9)
 
         anl.plot.plot_function(
             data=curve_data,
             func=anl.fit.cauchy,
             params=["x_0", "gamma", "I"],
-            domain=[3.5e9, 6.0e9],
-            rows=curve_data["phi"][::(len(curve_data["phi"]) // 5)],         # Extract sample rows.
+            domain=domain,
+            rows=rows,
+            overlay=[anl.read.AttributedData(
+                raw_data[(raw_data["f_RF"] >= domain[0]) & (raw_data["f_RF"] <= domain[1])],
+                x_var="f_RF",
+                y_vars=rows,
+                fmt='x'
+            )],
             xlabel="f_RF (Hz)",
             ylabel=f"fitted amp_{mag_var}",
             title=f"Curve-fit check for amp_{mag_var}",
             save_to=join(
                 anl.paths.plots_dir(DATE, ["checks"]),
-                f"check, amp_{mag_var} against f_RF.pdf"
+                f"check amp_{mag_var} against f_RF.pdf"
             )
         )
 
