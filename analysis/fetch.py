a31a76dfd21e28e0645abd6ecc70b9bb14bff9e2
qitianshi <65122416+qitianshi@users.noreply.github.com>
2021-12-31 11:32:16 +0800

    Add fetch_raw method

--- analysis/fetch.py
+++ analysis/fetch.py
@@ -2,8 +2,10 @@
 
 import requests
 
+from analysis import paths
 
 # Adapted from an answer by @turdus-merula on Stack Overflow: https://stackoverflow.com/a/39225272
+
 
 def __get_confirm_token(response):
     """Gets confirmation token from Google Drive to bypass large file
@@ -35,11 +37,19 @@ def __download_gdrive(drive_id, save_to):
 
     session = requests.Session()
 
-    response = session.get(url, params={'id': drive_id}, stream=True)
+    response = session.get(url, params={"id": drive_id}, stream=True)
     token = __get_confirm_token(response)
 
     if token:
-        params = {'id':drive_id, 'confirm':token }
+        params = {"id":drive_id, "confirm":token }
         response = session.get(url, params=params, stream=True)
 
     __save_response_content(response, save_to)
+
+
+def fetch_raw(date: str = None):
+    """Fetches large raw data files."""
+
+    with open(paths.raw_ref_path(date), "r", encoding="utf-8") as file:
+        drive_id = file.read().strip()
+        __download_gdrive(drive_id, paths.raw_txt_path())
