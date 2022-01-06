e6848a8230506bc26b543f0eddb48733d796221a
qitianshi <65122416+qitianshi@users.noreply.github.com>
2022-01-06 12:46:15 +0800

    Fix broken result_dir

--- analysis/paths/top.py
+++ analysis/paths/top.py
@@ -1,6 +1,7 @@
 """Top level of the results directory."""
 
 import os
+from pathlib import Path
 
 
 def latest_date() -> str:
@@ -20,7 +21,7 @@ def result_dir(date: str = None) -> str:
         str: The path of the result.
     """
 
-    results_path = os.path.join(os.path.dirname(__file__), os.pardir, 'results')
+    results_path = os.path.join(Path(__file__).parents[2], 'results')
     requested_result = date if date is not None else sorted(os.listdir(results_path))[-1]
 
     return os.path.join(results_path, requested_result)
