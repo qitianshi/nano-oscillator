892778f7fd46d1bd8f06306f43e151d39ca4f22a
qitianshi <65122416+qitianshi@users.noreply.github.com>
2022-01-08 11:26:18 +0800

    Add raw method

--- analysis/paths/data.py
+++ analysis/paths/data.py
@@ -5,6 +5,19 @@ import os
 from analysis.paths import top
 
 
+def raw(date: str = None, datum: str = None) -> str:
+    """Returns the path of data in the raw directory.
+     Args:
+        datum (str): The name of the file in the raw directory. If not given,
+          returns the root of the raw directory.
+     """
+
+    if datum is None:
+        return os.path.join(top.result_dir(date), "raw")
+    else:
+        return os.path.join(raw(date, datum=None), datum)
+
+
 def dataset_dir(date: str = None, vals: dict[str, str] = None) -> tuple[str, tuple[str]]:
     """Returns the path of the dataset split by the requested variables.
 
@@ -22,7 +35,7 @@ def dataset_dir(date: str = None, vals: dict[str, str] = None) -> tuple[str, tup
     """
 
     if vals is None:
-        return os.path.join(top.result_dir(date), "raw")
+        return raw(date, datum=None)
     else:
         return os.path.join(
             top.result_dir(date),
